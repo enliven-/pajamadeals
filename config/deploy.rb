7c8dac4b23e22525d68f091be0c7348ef35c2889
Bhushan Lodha <bhushanlodha@gmail.com>
2014-07-05 14:59:03 +0530

    Updated deploy.rb file to fetch repo from github

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,13 @@ end
 
 # Use a simple directory tree copy here to make demo easier.
 # You probably want to use your own repository for a real app
-set :scm, :none
-set :repository, "."
-set :deploy_via, :copy
+set :scm, :git
+set :repository, 'git@github.com:enliven-/pajamadeals.git'
+set :deploy_via, :remote_cache
+set :branch, 'passanger-nginx'
+
+ssh_options[:forward_agent] = true
+
 
 # Easier to do system level config as root - probably should do it through
 # sudo in the future.  We use ssh keys for access, so no passwd needed
