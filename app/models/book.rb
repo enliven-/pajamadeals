6a41838532d75daf114dad49b66b4bb3f82ec503
Bhushan Lodha <bhushanlodha@gmail.com>
2014-07-18 11:27:01 +0530

    Added validation on book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ActiveRecord::Base
   searchkick
 
+  validates :title, 	presence: true
+  validates :publisher, presence: true
+  validates :author, 	presence: true
+
   has_many :classifieds
   mount_uploader :image, ImageUploader
 end
