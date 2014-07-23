fcc4a68a5e77ea218d85c8e9de7411c464f9fe95
Bhushan Lodha <bhushanlodha@gmail.com>
2014-07-23 17:08:19 +0530

    cleaned up deprecated code from classified and user model refactored seed file

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -1,16 +1,13 @@
 class Classified < ActiveRecord::Base
   searchkick
 
-  validates :expected_price, presence: true, numericality: true
-  validates :retail_price,   numericality: true
+  validates :title, presence: true
+  validates :price, presence: true, numericality: true
 
   belongs_to :user
   accepts_nested_attributes_for :user
   belongs_to :college
   belongs_to :category
-
-  alias_attribute :selling_price, :expected_price
-  alias_attribute :mrp, :retail_price
 
   include HasToken
   has_token
