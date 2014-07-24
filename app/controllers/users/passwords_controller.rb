34e2d007df50957c71a2bf878b846efb31dca39f
Bhushan Lodha <bhushanlodha@gmail.com>
2014-07-25 02:13:38 +0530

    Fixed devise controllers

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,11 +1,5 @@
-class users::passwordsController < Devise::passwordsController
-  # def new
-  #   super
-  # end
+class Users::PasswordsController < Devise::passwordsController
 
-  # def create
-  #   super
-  # end
   protected
     def after_resetting_password_path_for(resource)
       signed_in_root_path(resource)
