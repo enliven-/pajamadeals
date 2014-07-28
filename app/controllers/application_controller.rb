12f87ad3bce3cdf34843a90604cdd52fd05447aa
Bhushan Lodha <bhushanlodha@gmail.com>
2014-07-28 15:32:53 +0530

    first finds college in session then for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
 
   def current_college
     return nil if session[:college_id] == 0 || session[:college_id] == '0'
-    @current_college = current_user.try(:college) ||
-      (College.find(session[:college_id]) if session[:college_id].present?)
+    @current_college = 
+      (College.find(session[:college_id]) if session[:college_id].present?) ||
+      current_user.try(:college)
   end
   helper_method :current_college
 
