6238df4e60fae5555860d336e5e69a8c6b1c280c
Bhushan Lodha <bhushanlodha@gmail.com>
2014-08-04 19:44:55 +0530

    activeadmin: permits params for user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register User do
 
-  
+
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :email, :encrypted_password, :college_id, :college, :name, :mobile
+
   #
   # or
   #
@@ -13,5 +14,5 @@ ActiveAdmin.register User do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
 end
