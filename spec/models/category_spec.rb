1e36a6f98a73ca98bf2ec878bde0a9b10068a491
Bhushan Lodha <bhushanlodha@gmail.com>
2014-08-22 17:57:03 +0530

    added category spec

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Category do
+  
+  it 'should have name' do
+    category = Category.new
+    expect(category).to respond_to(:name)
+  end
+  
+  it 'has valid factories' do
+    category = build :category
+    expect(category).to be_valid
+    expect(category.save).to be_truthy
+    
+    category = build :books
+    expect(category).to be_valid
+    expect(category.save).to be_truthy
+    
+    category = build :mobiles
+    expect(category).to be_valid
+    expect(category.save).to be_truthy
+    
+    category = build :projects
+    expect(category).to be_valid
+    expect(category.save).to be_truthy
+  end
+end
