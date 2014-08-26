73e7ac55ea798b2c3b42b9972c06e04ebbe32182
Bhushan Lodha <bhushanlodha@gmail.com>
2014-08-26 21:36:15 +0530

    removed unused model

--- app/models/image.rb
+++
@@ -1,5 +0,0 @@
-class Image < ActiveRecord::Base
-	belongs_to :classified
-
-	mount_uploader :file, ImageUploader
-end
--- spec/factories/images.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :image do
-    file "MyString"
-    imageable_type "MyString"
-    imageable_id 1
-  end
-end
