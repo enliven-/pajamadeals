6810864492079d02d7cc7c8b6c7c4cfe71935aa9
Bhushan Lodha <bhushanlodha@gmail.com>
2014-08-26 23:08:43 +0530

    Created two types of classifieds - buy and sell

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -131,7 +131,7 @@ class ClassifiedsController < ApplicationController
 
   def classified_params
     params.require(:classified).permit(:title, :description, :category_id,
-                                       :price,
+                                       :price, :listing_type,
                                        user_attributes: [:email, :mobile,
                                                          :name, :college_id,
                                                          :password]
