c8b7b9c30b9323684e1219d0876760373d83fe8d
Bhushan Lodha <bhushanlodha@gmail.com>
2014-09-08 09:09:50 +0530

    Removed email index from users, email is not compulsory now

--- db/migrate/20140906171911_remove_devise_fields_from_user.rb
+++ db/migrate/20140906171911_remove_devise_fields_from_user.rb
@@ -1,15 +1,12 @@
 class RemoveDeviseFieldsFromUser < ActiveRecord::Migration
   def change
     remove_index :users, :reset_password_token
+    remove_index :users, :email
 
     remove_column :users, :encrypted_password
     remove_column :users, :reset_password_token
     remove_column :users, :reset_password_sent_at
     remove_column :users, :remember_created_at
     remove_column :users, :guest, :boolean
-    
-    User.all.each do |user|
-      user.update_attributes email: "devise.#{user.email}"
-    end
   end
 end
