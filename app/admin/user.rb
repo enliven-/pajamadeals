150335810091290ec1b1846d99b8732bc14c295d
Bhushan Lodha <bhushanlodha@gmail.com>
2014-09-08 19:33:37 +0530

    Can edit role via admin interface

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,7 +4,8 @@ ActiveAdmin.register User do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :email, :encrypted_password, :college_id, :college, :name, :mobile
+  permit_params :email, :encrypted_password, :college_id, :college, :name,
+                :mobile, :role
 
   #
   # or
