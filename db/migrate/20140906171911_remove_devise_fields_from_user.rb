bd800a4b6afe6a4040d53ae88f92abe2399a797e
Bhushan Lodha <bhushanlodha@gmail.com>
2014-09-08 11:22:14 +0530

    Added index on uid and mobile

--- db/migrate/20140906171911_remove_devise_fields_from_user.rb
+++ db/migrate/20140906171911_remove_devise_fields_from_user.rb
@@ -8,5 +8,8 @@ class RemoveDeviseFieldsFromUser < ActiveRecord::Migration
     remove_column :users, :reset_password_sent_at
     remove_column :users, :remember_created_at
     remove_column :users, :guest, :boolean
+    
+    add_index :users, :mobile, unique: true
+    add_index :users, :uid, unique: true
   end
 end
