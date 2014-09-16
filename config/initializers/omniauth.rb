58f2f1a28aecbc5ee5da0b6b83cf87a680037030
Bhushan Lodha <bhushanlodha@gmail.com>
2014-09-17 01:32:22 +0530

    Omniauth credentials for development

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,12 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
  # provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
- provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
+ if Rails.env.production?
+   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
                      scope: 'email'
+ elsif Rails.env.development?
+   provider :facebook, '1496050790612397', '8527f91e1fe1095a59c8564803863a7c',
+   scope: 'email'
+ end
+                     
 end
