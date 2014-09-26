c6b30374d69fc51fc9512a0ac10089f3345704fe
Bhushan Lodha <bhushanlodha@gmail.com>
2014-09-26 12:27:40 +0530

    Doesn't require image and price for 'want' listing

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -3,9 +3,6 @@ class Classified < ActiveRecord::Base
   
   enum listing_type: { buy: 0, sell: 1 }
 
-  validates :title, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-
   belongs_to :user
   accepts_nested_attributes_for :user
   belongs_to :college
