72835c6b8ecaf6a948f2d7bd099bdfc18377cd9e
Bhushan Lodha <bhushanlodha@gmail.com>
2014-09-26 12:57:16 +0530

    changed buy label text to want

--- app/helpers/classifieds_helper.rb
+++ app/helpers/classifieds_helper.rb
@@ -6,7 +6,7 @@ module ClassifiedsHelper
   
   def render_listing_type_label(classified)
 		if classified.buy?
-			'<div class="label label-success">Buy</div>'.html_safe
+			'<div class="label label-success">Want</div>'.html_safe
 		else
 			'<div class="label label-info">Sell</div>'.html_safe
     end
