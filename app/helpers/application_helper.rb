56e407edd855f3c6df53571b24cc254ef200f976
Bhushan Lodha <bhushanlodha@gmail.com>
2014-10-16 13:58:57 +0530

    working on render_menu function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,9 +29,23 @@ module ApplicationHelper
     content_tag(:i, '', options) + (nosep ? '' : ' ')
   end
   
-  def render_menu(options, &block)
-    
-  end
+  # def render_menu(option = {}, &block)
+ #    "<nav id='myNavmenu' class='navmenu navbar-default navmenu-default navmenu-fixed-right offcanvas' role='navigation'>
+ #      <button type='button' class='navbar-toggle' data-toggle='offcanvas' data-target='#myNavmenu' data-canvas='body'>
+ #        <span class='icon-bar'></span>
+ #        <span class='icon-bar'></span>
+ #        <span class='icon-bar'></span>
+ #      </button>
+ #        #{ link_to App.logo, root_path, class: 'navmenu-brand' }
+ #          <ul class='nav navmenu-nav'>
+ #            <li>#{ link_to 'Post an Ad (free)', new_classified_path }</li>
+ #          </ul>
+ #        <hr class='hr-dashed'></hr>
+ #        #{ yield }
+ #        <hr class='hr-dashed'></hr>
+ #        #{ render partial: 'layouts/user_session_links' }
+ #     </nav".html_safe
+ #  end
  
 
 end
